a16445a9ef7766dfd77e979acb0e81266628e8da
Baozi960101 <Baozi960101@gmail.com>
2021-05-11 00:05:14 +0800

    first commit

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function handlePriceStr(lastWTF) {
         str = str + c.innerHTML
     })
     return str.replace('undefined', '-')
-  }
-  
-  function grabData() {
+}
+
+function grabData() {
     const height = Math.floor(document.body.scrollHeight / 4)
     for (let i = 1; i < 6; i++) {
         setTimeout(() => {
@@ -19,29 +19,29 @@ function handlePriceStr(lastWTF) {
             const res = []
             setTimeout(() => {
                 const temp = []
-  
+
                 const name = document.querySelectorAll('.PFM7lj')
                 name.forEach((item, index) => {
                     temp[index] = {}
                     const moreName = item.childNodes[item.childNodes.length - 1].innerHTML
                     temp[index].title = moreName
                 })
-  
+
                 const allImages = document.querySelectorAll('.mxM4vG')
                 allImages.forEach((image, index) => {
                     temp[index].image = image.src
                 })
-  
+
                 const all32 = document.querySelectorAll("._32hnQt")
                 all32.forEach((item32, index) => {
                     const arr = []
-  
+
                     item32.childNodes.forEach(c => {
                         if (c.classList.contains('WTFwws')) {
                             arr.push(c)
                         }
                     })
-  
+
                     const lastWTF = arr[arr.length - 1]
                     temp[index].price = handlePriceStr(lastWTF)
                 })
@@ -51,18 +51,18 @@ function handlePriceStr(lastWTF) {
             }, 5000)
         }
     }
-  }
-  
-  function main() {
-  
+}
+
+function main() {
+
     grabData()
-  
+
     setInterval(() => {
         const btn = document.querySelector(".shopee-icon-button--right")
         btn.click()
         grabData()
     }, 8000)
-  }
-  
-  // 啟動主程式
-  main()
+}
+
+// 啟動主程式
+main()
